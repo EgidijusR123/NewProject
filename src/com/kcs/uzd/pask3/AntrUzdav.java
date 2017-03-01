7305285738d95b02d0e65acfc198533364d4b899
EgidijusR123 <egidijusrubys@gmail.com>
2017-03-01 21:47:53 +0200

    Judam toliau sekmingai

---
+++ src/com/kcs/uzd/pask3/AntrUzdav.java
@@ -0,0 +1,48 @@
+package com.kcs.uzd.pask3;
+
+import com.kcs.uzd.pask3.WO.Car;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created by Egidijus on 3/1/2017.
+ */
+public class AntrUzdav
+{
+    public static void main(String[] args)
+    {
+        Set<Car> cars=new TreeSet<>();//HashSet
+        readFile("Cars.txt",cars);
+        printSet(cars);
+    }
+    private static void printSet(Set<Car>cars)
+    {
+        for(Car car:cars){
+            System.out.println(car);
+        }
+    }
+    private static void readFile(String fileName,Set<Car>cars)
+    {
+        try {
+            BufferedReader bf=new BufferedReader(new FileReader(new File(fileName)));
+            String line=null;
+            while ((line=bf.readLine())!=null)
+            {
+                addCar(line,cars);
+            }
+        } catch (IOException e) {
+            System.out.println("Kazkas Blogai");
+        }
+    }
+    private static void addCar(String line, Set<Car> cars)
+    {
+        String[] items=line.split("\t");
+        if(items.length==3)
+        {
+            cars.add(new Car(items[0],items[1],(items[2])));
+        }
+    }
+}
---
+++ src/com/kcs/uzd/pask3/WO/Car.java
@@ -0,0 +1,90 @@
+package com.kcs.uzd.pask3.WO;
+
+import java.util.Comparator;
+
+/**
+ * Created by Egidijus on 3/1/2017.
+ */
+public class Car implements Comparable<Car>
+{
+    private String model;
+    private String year;
+    private double carPrice;
+
+   /* public Car(String model, String year,double price){
+        this.model=model;
+        this.year=year;
+        this.carPrice=price;
+    }*/
+
+    public Car(String model, String year,String cprice){
+        this.model=model;
+        this.year=year;
+        this.carPrice=Double.parseDouble(cprice);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (Double.compare(car.carPrice, carPrice) != 0) return false;
+        if (!model.equals(car.model)) return false;
+        return year.equals(car.year);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = model.hashCode();
+        result = 31 * result + year.hashCode();
+        temp = Double.doubleToLongBits(carPrice);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public double getCarPrice() {
+        return carPrice;
+    }
+
+    public void setCarPrice(double carPrice) {
+        this.carPrice = carPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", year='" + year + '\'' +
+                ", carPrice=" + carPrice +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Car o) {
+       // return model.compareTo(o.getModel());
+        if(carPrice>o.carPrice)
+            return -1;
+        else if(carPrice<o.getCarPrice())return 1;
+        else return 0;
+    }
+}
